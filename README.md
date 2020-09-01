5b9208de4295e97f799b390e9cbe212c015af4fb
Muhammad Abid <abidaks@users.noreply.github.com>
2020-09-01 22:22:21 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# TensorFlow Speech Recognition Challenge
+This repo contains the code for winning entry in the [TensorFlow Speech Recognition Challenge](https://www.kaggle.com/c/tensorflow-speech-recognition-challenge) hosted by [kaggle](https://www.kaggle.com). The task was to *build an algorithm that understands simple speech commands*.
+
+The score of this model is: 0.91060.
+
+
+# Appendix
+##  A1.) Model Execution Time
+- What software did you use for training and prediction?
+  - see Requirements
+- What hardware (CPUS spec, number of CPU cores, memory)?
+  - a single GCP instance with 4 vCPUs, 18.5 GB memory and 
+1 x NVIDIA Tesla K80
+- How long does it take to train your model?
+  - ~4-8 hours (depends on the model) for a single model
+-
+
+
+## A2.) Requirements:
+- tensorflow>=2.3.0
+- tensorflow-gpu>=2.3.0
+- Keras>=2.4.3
+- tqdm>=4.48.0
+- scipy>=1.4.1
+- numpy>=1.19.1
+- pandas>=0.24.2
+- pandas-ml>=0.6.1
+
+All packages can be installed via `pip install`. Other versions will probably work too. I tested it with Python 3.7 using Ubuntu 20.04.
+
+Note that installing scipy will take a couple hours. I tried removing this dependency by using the `wave` and `struct` modules but then reading the wavs is super slow (2 hours for all test files).
+I tested it with Raspbian GNU/Linux 8 (Jessie) and Python 3.4.2.
+
+## Installing dependencies
+
+run below command to install dependencies
+```
+> git clone https://github.com/abidaks/speech-recognition
+> cd speech-recognition
+> pip install requirements.txt
+```
+
+## Downloading dataset
+Before training the model you need to download the training dataset from [Kaggle](https://www.kaggle.com/c/tensorflow-speech-recognition-challenge/data?select=train.7z) and place it under `/data/`.
+
+
+## Model training
+Run the jupyter notebook `trainer.ipynb` to train the model.
+
+
---
+++ loadmodel.py
@@ -0,0 +1,77 @@
+from __future__ import division, print_function
+import tensorflow as tf
+from tensorflow.compat.v1.keras import backend as K
+from tensorflow.compat.v1.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.compat.v1.keras.callbacks import TensorBoard
+from callbacks import ConfusionMatrixCallback
+from model import speech_model, prepare_model_settings
+from input_data import AudioProcessor, prepare_words_list
+from classes import get_classes
+from utils import data_gen
+from IPython import embed  # noqa
+
+
+# running_mean: -0.8 | running_std: 7.0
+# mfcc running_mean: -0.67 | running_std: 7.45
+# background_clamp running_mean: -0.00064 | running_std: 0.0774, p5: -0.074, p95: 0.0697  # noqa
+
+# np.log(12) ~ 2.5
+# np.log(32) ~ 3.5
+# np.log(48) ~ 3.9
+# 64727 training files
+if __name__ == '__main__':
+  # restrict gpu usage: https://stackoverflow.com/questions/34199233/how-to-prevent-tensorflow-from-allocating-the-totality-of-a-gpu-memory  # noqa
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+  sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+  K.set_session(sess)
+  data_dirs = ['data/train/audio']
+  add_pseudo = True
+  if add_pseudo:
+    data_dirs.append('data/heng_pseudo')
+  output_representation = 'raw'
+  sample_rate = 16000
+  batch_size = 384
+  classes = get_classes(wanted_only=True, extend_reversed=False)
+  model_settings = prepare_model_settings(
+      label_count=len(prepare_words_list(classes)), sample_rate=sample_rate,
+      clip_duration_ms=1000, window_size_ms=30.0, window_stride_ms=10.0,
+      dct_coefficient_count=80, num_log_mel_features=60,
+      output_representation=output_representation)
+  ap = AudioProcessor(
+      data_dirs=data_dirs, wanted_words=classes,
+      silence_percentage=13.0, unknown_percentage=60.0,
+      validation_percentage=10.0, testing_percentage=0.0,
+      model_settings=model_settings,
+      output_representation=output_representation)
+  train_gen = data_gen(ap, sess, batch_size=batch_size, mode='training',
+                       pseudo_frequency=0.6)
+  val_gen = data_gen(ap, sess, batch_size=batch_size, mode='validation',
+                     pseudo_frequency=0.0)
+  model = speech_model(
+      'conv_1d_time_sliced_with_attention',
+      model_settings['fingerprint_size'] if output_representation != 'raw' else model_settings['desired_samples'],  # noqa
+      num_classes=model_settings['label_count'],
+      **model_settings)
+  # embed()
+  
+
+  callbacks = [
+      ConfusionMatrixCallback(
+          val_gen, ap.set_size('validation') // batch_size,
+          wanted_words=prepare_words_list(get_classes(wanted_only=True)),
+          all_words=prepare_words_list(classes),
+          label2int=ap.word_to_index),
+      ReduceLROnPlateau(monitor='val_categorical_accuracy', mode='max',
+                        factor=0.5, patience=4, verbose=1, min_lr=1e-5),
+      TensorBoard(log_dir='logs_210'),
+      ModelCheckpoint(
+          'checkpoints_210/ep-{epoch:03d}-vl-{val_loss:.4f}.hdf5',
+          save_best_only=True, monitor='val_categorical_accuracy',
+          mode='max')]
+  model.fit_generator(
+      train_gen, steps_per_epoch=ap.set_size('training') // batch_size,
+      epochs=100, verbose=1, callbacks=callbacks)
+
+  eval_res = model.evaluate_generator(
+      val_gen, ap.set_size('validation') // batch_size)
+  print(eval_res)
